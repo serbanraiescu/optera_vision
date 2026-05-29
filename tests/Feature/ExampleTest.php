490fddf9dc58d91a8dfd0dd06f818cb655ecefbe
serbanraiescu <serbanraiescu@gmail.com>
2026-05-29 13:13:18 +0300

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,16 +2,21 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        // Run database seeders to populate the required cached homepage and settings
+        $this->seed();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
